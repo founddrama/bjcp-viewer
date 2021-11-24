208a8ce0c349102666d9b06b9c5c26ae3668e483
founddrama <>
2021-11-24 01:28:34 +0000

    Deploying to gh-pages from @ founddrama/bjcp-viewer@30eb75fd39f49255571fa7a70e9bbf4752fb50ea 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "bjcp-viewer/precache-manifest.b44d7b6346399ea69dd2ba3139f2df4a.js"
+  "/bjcp-viewer/precache-manifest.beea8b435f0e134364f0dfce9a77541a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("bjcp-viewer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bjcp-viewer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
