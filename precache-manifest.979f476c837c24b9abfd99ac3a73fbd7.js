80f995e1beae288be5db04d9725c6891639ad3e3
founddrama <>
2021-11-24 18:07:14 +0000

    Deploying to gh-pages from @ founddrama/bjcp-viewer@c67c1f96cf08fd9d84a509ea47ca77c22e45df82 🚀

---
+++ precache-manifest.979f476c837c24b9abfd99ac3a73fbd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a5d95e60f756e0c9883684fd08d332",
+    "url": "https://founddrama.github.io/bjcp-viewer/index.html"
+  },
+  {
+    "revision": "fb340569c4268846f6c7",
+    "url": "https://founddrama.github.io/bjcp-viewer/static/css/main.dccf1b18.chunk.css"
+  },
+  {
+    "revision": "1380616a1fea75353b5a",
+    "url": "https://founddrama.github.io/bjcp-viewer/static/js/2.b3d88df0.chunk.js"
+  },
+  {
+    "revision": "adb6d8dd7d0dae9263e848417cd813ec",
+    "url": "https://founddrama.github.io/bjcp-viewer/static/js/2.b3d88df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb340569c4268846f6c7",
+    "url": "https://founddrama.github.io/bjcp-viewer/static/js/main.87a63e36.chunk.js"
+  },
+  {
+    "revision": "d76500f59e8da76edf17",
+    "url": "https://founddrama.github.io/bjcp-viewer/static/js/runtime-main.7f5f26c2.js"
+  }
+]);
