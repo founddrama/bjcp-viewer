a61b6df4e426a140efeab41a2a98b6aed4d5b0b2
Rob Friesel <rob@founddrama.net>
2021-11-24 19:40:56 -0500

    fix: <Router basename> to address #3

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import './css/bjcp.css';
 const styles = require('./bjcp/2015-bjcp-styleguide-flattened.json');
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <StyleGuideApp styles={styles} />
   </Router>,
   document.getElementById('root')
